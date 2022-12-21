27ba8ea1d3c09fe43e43cfda6a936c4b9e4645d5
MuntasirSunny <hossainmdmuntasir@gmail.com>
2022-12-22 01:55:16 +0600

    vector-update-motion

--- sections/GetitSection.js
+++ sections/GetitSection.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 
 function GetitSection() {
@@ -7,17 +8,23 @@ function GetitSection() {
             <div className="cta-inner section-inner">
             <div className="cta-header text-center">
                 <h2 className="section-title mt-0">
-                Get it and Switch
+                যোগ্যতা যাচাই করুন
                 </h2>
                 <p className="section-paragraph">
-                Lorem ipsum is common placeholder text used
-                to demonstrate the graphic elements of a
-                document or visual presentation.
+                আর এম ডেলিভারি সার্ভিসেস এল.এল.সি এর সাথে ফুড ডেলিভারি বাইক রাইডার হিসেবে যোগদান করতে এখনই আপনার যোগ্যতা যাচাই করুন 
                 </p>
                 <div className="cta-cta">
-                <a className="button button-primary" href="#"
-                    >Buy it now</a
+                <motion.button
+                    whileHover={{
+                        scale: 1.07,
+                        transition: { duration: 0.5 },
+                    }}
+                    whileTap={{ scale: 0.7 }}
+                    className="button button-primary"
+                    onClick={()=>console.log('clicked!')}
                 >
+                    Check Now
+                </motion.button>
                 </div>
             </div>
             </div>
